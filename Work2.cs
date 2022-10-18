aedce355ff0a4c3880bca547a27cdcd8147e6a55
GrigoriyLeb <114830136+GrigoriyLeb@users.noreply.github.com>
2022-10-18 22:48:34 +0300

    Задача 2

---
+++ Work2.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine()!);
+
+if (number1 > number2 && number1 > number3)
+{
+    Console.Write("Первое число больше");
+}
+
+else if (number2 > number1 && number2 > number3)
+{
+    Console.Write("Второе число больше");
+}
+else if(number3 > number2 && number3 > number1){
+    Console.Write("Третье число больше");
+}
